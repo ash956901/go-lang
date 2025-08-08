34eca842176f52ea4aaf37cc600beec79c843f39
ashutosh <ak956901@gmail.com>
2025-08-08 16:10:03 +0530

    ifelse

---
+++ 6_if_else/ifelse.go
@@ -0,0 +1,40 @@
+package main
+
+func main() {
+
+	//if-else
+	// age:=16
+	// if age>=18 {
+	// 	fmt.Println("person is adult")
+	// } else{
+	// 	fmt.Println("person is not an adult")
+	// }
+
+	//if else-if
+	// age := 11
+	// if age >= 18 {
+	// 	fmt.Println("person is adult")
+	// } else if age >= 12 {
+	// 	fmt.Println("person is teenager")
+	// } else {
+	// 	fmt.Println("person is kid")
+	// }
+
+	//logical operators use || &&
+	// var role="admin"
+	// var hasPermissions=false
+
+	// if role == "admin" && hasPermissions {
+	// 	fmt.Println("yes")
+	// }
+
+	//variable declare within if ,can use within the construct
+	// if age:=15; age>=18{
+	// 	fmt.Println("person is an adult",age)
+	// }else if age>=12 {
+	// 	fmt.Println("person is an teenager",age)
+	// }
+
+	//Note: Go does not have ternary operator , you will have to use normal if-else
+
+}
